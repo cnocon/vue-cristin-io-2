e4923f2dc5de1f09ded1258149c174bead7aa3e6
Cristin O'Connor <her@cristin.io>
2021-08-17 00:31:42 -0600

    Fixed invalid quote prop type in scss blog post, updated nuxt.config with server side rendering

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // CONFIG TARGET: https://go.nuxtjs.dev/config-target
   // 'server': For server side rendering and 'static': For static sites
-  target: 'static',
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
